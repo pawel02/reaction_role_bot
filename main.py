5f1290380f60ac675091c42801f47cc0c7cb92e5
Senior dev (GOAT) <pawel.bes13@gmail.com>
2022-02-24 12:26:25 +0000

    fixed issues

--- main.py
+++ main.py
@@ -3,30 +3,29 @@ from discord.utils import get
 import os
 
 client = discord.Client()
-# id of the channel you have setup as your verify channel
-verify_channel_id = 0
 
+# Get the id of the verify channel
 @client.event
 async def on_ready():
-    # get the verify channel id
     for guild in client.guilds:
         for channel in guild.text_channels:
-            if channel == "verify":
+            if str(channel).strip() == "verify":
+                # id of the channel you have setup as your verify channel
+                global verify_channel_id
                 verify_channel_id = channel.id
                 break
-                
 
 # Called when a reaction is added to a message
-@client.event
+@client.event 
 async def on_raw_reaction_add(reaction):
     # check if the reaction came from the correct channel
     if reaction.channel_id == verify_channel_id:
         # Check what emoji was reacted as
-        if reaction.emoji == "👍":
-            # Add the user role
-            verfied_role = get(reaction.server.roles, "verified")
-            await client.add_roles(reaction.user_id, verfied_role)
-    
-    
-        
-client.run(os.getenv("TOKEN"))
+        if str(reaction.emoji) == "👍":
+             # Add the user role
+            verified_role = get(reaction.member.guild.roles, name = "verified")
+            await reaction.member.add_roles(verified_role)
+
+        # Add your other roles here
+
+client.run(os.getenv("TOKEN")) 
